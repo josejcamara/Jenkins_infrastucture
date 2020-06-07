5701b9f51587f26e38c98931c5d2e8ec1ff59135
josejcamara <josejcamara@gmail.com>
2020-06-07 22:55:24 +0100

    Use attributes file

--- chef/jenkins-master/metadata.rb
+++ chef/jenkins-master/metadata.rb
@@ -7,6 +7,7 @@ version '0.1.0'
 chef_version '>= 14.0'
 
 depends 'apt'
+depends 'jenkins', '7.1.2'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- chef/jenkins-master/recipes/default.rb
+++ chef/jenkins-master/recipes/default.rb
@@ -4,24 +4,37 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
-# Instalation steps from:
-# https://youtu.be/B9zhtyIazzM
-# https://www.jenkins.io/doc/book/installing/#debianubuntu
-
 include_recipe 'apt::default'
 
-apt_repository 'jenkins' do
-    uri   'https://pkg.jenkins.io/debian-stable' 
-    distribution 'binary/'
-    key   'https://pkg.jenkins.io/debian-stable/jenkins.io.key' 
-  end
-
-# Found this issue with the installation
-# https://issues.jenkins-ci.org/browse/JENKINS-31814
+# include_recipe 'java'
 package 'openjdk-8-jre-headless'
 
-# sudo apt-get install jenkins
-package 'jenkins' do
-    version '2.222.3'
-    action :install
-end
+package 'unzip'
+
+# if node['jenkins-server']['nginx']['install']
+#   include_recipe 'jenkins-server::nginx'
+# end
+
+include_recipe 'jenkins::master'
+
+# include_recipe 'jenkins-master::settings'
+
+# include_recipe 'jenkins-master::user'
+# include_recipe 'jenkins-master::plugins'
+
+# include_recipe 'jenkins-master::security'
+# include_recipe 'jenkins-server::views'
+# include_recipe 'jenkins-server::jobs'
+# include_recipe 'jenkins-server::composer'
+
+# if node['jenkins-server']['slaves']['enable']
+#   include_recipe 'jenkins-server::slaves_credentials'
+#   include_recipe 'jenkins-server::slaves'
+# end
+
+# After finishing any config, it's needed to restart for running init scripts
+# jenkins_command 'safe-restart'
+# Remove folder of init scripts
+# directory "#{node['jenkins']['master']['home']}/init.groovy.d" do
+#   action :delete
+# end
