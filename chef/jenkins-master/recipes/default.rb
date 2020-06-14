4ab3a5f7de9910389d3bdc43d11b0210e374c7d4
josejcamara <josejcamara@gmail.com>
2020-06-14 23:12:03 +0100

    First functional recipe

--- chef/jenkins-master/recipes/default.rb
+++ chef/jenkins-master/recipes/default.rb
@@ -15,64 +15,8 @@ include_recipe 'apt::default'
 # include_recipe 'java'
 package 'openjdk-8-jre-headless'
 
-###### attributes -> default['jenkins']['http_proxy']...
-# if node['jenkins-server']['nginx']['install']
-#   include_recipe 'jenkins-server::nginx'
-# end
-
 include_recipe 'jenkins::master'
 
-JENKINS_HOME = node['jenkins']['master']['home']
-SSL_DIR = File.join(JENKINS_HOME, "ssl")
-SSH_DIR = File.join(JENKINS_HOME, ".ssh")
-[ SSL_DIR, SSH_DIR ].each do |dir_name|
-    directory dir_name do
-        owner node['jenkins']['master']['user']
-        group node['jenkins']['master']['group']
-        mode '0700'
-        recursive true
-    end
-end
+include_recipe 'jenkins-master::plugins'
+include_recipe 'jenkins-master::users'
 
-# Create private key file
-file File.join("#{SSH_DIR}","id_rsa") do
-    content node['jenkins-master']['dev_mode']['security']['private_key']
-    owner node['jenkins']['master']['user']
-    group node['jenkins']['master']['group']
-    mode 0600
-    action :create
-end
-
-# Create public key file
-file File.join("#{SSH_DIR}","id_rsa.pub") do
-    content node['jenkins-master']['dev_mode']['security']['public_key']
-    owner node['jenkins']['master']['user']
-    group node['jenkins']['master']['group']
-    mode 0644
-    action :create
-    # notifies :create, "ruby_block[store_ssh_public_key]", :immediately
-end
-
-
-
-# include_recipe 'jenkins-master::settings'
-
-# include_recipe 'jenkins-master::user'
-# include_recipe 'jenkins-master::plugins'
-
-# include_recipe 'jenkins-master::security'
-# include_recipe 'jenkins-server::views'
-# include_recipe 'jenkins-server::jobs'
-# include_recipe 'jenkins-server::composer'
-
-# if node['jenkins-server']['slaves']['enable']
-#   include_recipe 'jenkins-server::slaves_credentials'
-#   include_recipe 'jenkins-server::slaves'
-# end
-
-# After finishing any config, it's needed to restart for running init scripts
-# jenkins_command 'safe-restart'
-# Remove folder of init scripts
-# directory "#{node['jenkins']['master']['home']}/init.groovy.d" do
-#   action :delete
-# end
---
+++ chef/jenkins-master/recipes/plugins.rb
@@ -0,0 +1,25 @@
+Chef::Log.debug '[JENKINS] Configuring Plugins...'
+
+# This resource does not install plugin dependencies from a a given hpi/jpi URL or a specific version
+# - you must specify all plugin dependencies or Jenkins may not startup correctly!
+
+plugins_list = [
+  {'name' => 'mailer', 'version' => '1.32', 'deps' => [] },
+  {'name' => 'credentials', 'version' => '2.3.8', 'deps' => [] },
+  {'name' => 'jdk-tool', 'version' => '1.4', 'deps' => [] },
+  {'name' => 'token-macro', 'version' => '2.12', 'deps' => [] },
+  {'name' => 'durable-task', 'version' => '1.34', 'deps' => [] },
+  {'name' => 'docker-plugin', 'version' => '1.2.0', 'deps' => ['docker-commons', 'ssh-slaves', 'apache-httpcomponents-client-4-api', 'docker-java-api', 'bouncycastle-api'] },
+  {'name' => 'simple-theme-plugin', 'version' => '0.6', 'deps' => [] }
+]
+
+plugins_list.each do |plugin|
+  plugin['deps'].each do |dep|
+    jenkins_plugin dep
+  end
+  jenkins_plugin plugin['name'] do
+    version plugin['version']
+  end
+end
+
+# TODO: Restart to apply changes
